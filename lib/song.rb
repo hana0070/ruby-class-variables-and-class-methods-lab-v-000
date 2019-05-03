459df275f63545f29dd3c0cf219d2eb248d3a83d
HANA LEANDER <hana0070@gmail.com>
2019-05-03 21:43:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 class Song
   attr_accessor :name, :genre, :artist
 
-  @@song_count = 0
+  @@count = 0
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@song_count += 1
+    @@count += 1
   end
 
 end
