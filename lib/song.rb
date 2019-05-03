f4d40f85a944fbe4a23c4dc4128c153f8b105b08
HANA LEANDER <hana0070@gmail.com>
2019-05-03 21:55:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artist << artist
-    @@genre << genre
+    @@artists << artist
+    @@genres << genre
   end
 
   def self.count
