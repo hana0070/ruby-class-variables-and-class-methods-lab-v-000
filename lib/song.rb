49c47325c12ec50b79f2ef0e4c7f25f1c78d931b
HANA LEANDER <hana0070@gmail.com>
2019-05-03 22:19:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
   end
 
   def self.genre_count
-    genre_count = []
+    genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
         genre_count += 1
@@ -58,7 +58,7 @@ def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count[genre] += 1 
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
